0d01eabd1a0c3c88b9b979a4ef5ef941fc3552a0
ben <wxb2hw@gmail.com>
2020-05-29 23:21:22 +0800

    服务 、模块 、中间件、异常过滤器的使用

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DemoFilter } from './core/filters/demo.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalFilters(new DemoFilter()); // 全局级过滤器
   await app.listen(3000);
 }
 bootstrap();
---
+++ src/modules/posts/posts.controller.ts
@@ -0,0 +1,48 @@
+import {
+  Controller,
+  Get,
+  Param,
+  Post,
+  Body,
+  HttpException,
+  HttpStatus,
+  ForbiddenException,
+  UseFilters,
+} from '@nestjs/common';
+import { CreatePost } from './post.dto';
+import { DemoService } from './providers/demo/demo.service';
+import { throws } from 'assert';
+import { DemoFilter } from '../../core/filters/demo.filter';
+
+@Controller('posts')
+// @UseFilters(DemoFilter) // 模块级过滤器
+export class PostsController {
+  // private readonly demoService: DemoService;
+  // constructor(demoService: DemoService){
+  //   this.demoService = demoService;
+  // }
+
+  constructor(private readonly demoService: DemoService) {}
+
+  @Get()
+  index() {
+    return this.demoService.findAll();
+  }
+
+  @Get(':id')
+  show(@Param() params) {
+    console.log(params);
+    return {
+      title: `Post ${params.id}`,
+      subtitle: 'subtitle',
+    };
+  }
+
+  @Post()
+  // @UseFilters(DemoFilter) // 方法级过滤器
+  store(@Body() post: CreatePost) {
+    // throw new HttpException('没有权限', HttpStatus.FORBIDDEN);
+    throw new ForbiddenException('没有权限');
+    // this.demoService.create(post);
+  }
+}
