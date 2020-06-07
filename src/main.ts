ed8fdb5d46814b90453a623ac9e255764eac5e2c
ben <wxb2hw@gmail.com>
2020-06-07 21:55:33 +0800

    update

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { DemoFilter } from './core/filters/demo.filter';
-import { LoggingInterceptor } from './core/interceptors/logging.interceptor';
-import { DemoAuthGuard } from './core/guards/demo-auth.guard';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // app.useGlobalFilters(new DemoFilter()); // 全局级过滤器
-  // app.useGlobalGuards(new DemoAuthGuard()); // 全局守卫
-  // app.useGlobalInterceptors(new LoggingInterceptor()); // 全局拦截器
   await app.listen(3000);
 }
 bootstrap();
